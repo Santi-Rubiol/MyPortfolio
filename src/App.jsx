b00e85efe56f39c1c20e8cf1511537f921fc56f9
SantiRubiol <santirubiol14@gmail.com>
2024-04-26 13:38:51 -0300

    Estructuro mejor el Layout de la app, agrego datos al json

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Experience from "./components/Experience"
 import Skill from "./components/Skill"
 import Contact from "./components/Contact"
 import wppLogo from './assets/whatsapp-logo.svg'
 import gmailLogo from './assets/gmail-logo.svg'
 import linkedinLogo from './assets/linkedin-logo.svg'
 import gitHubLogo from './assets/github-logo.svg'
+import data from '../data.json'
 
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
 
 function App() {
@@ -14,45 +16,90 @@ function App() {
   const cuerpoFormateado = encodeURIComponent("Hola Santiago!")
   const urlCorreo = `mailto:${"santirubiol14@gmail.com"}?subject=${asuntoFormateado}&body=${cuerpoFormateado}`
 
+  const skills = data.skills
+  // const contacts = data.contacts
+
 
   return (
-    <div className="principalPage">
-      <div className="headerPrincipalData">
-        <h1 className="myName">SANTIAGO RUBIOL</h1>
-        <h2 className="myTitle">FULL STACK DEVELOPER <b>(STACK MERN)</b></h2>
-      </div>
-      <div className="headerContactData">
-        <p>Tafí Viejo, Tucumán, Argentina</p>
-        <Contact
-          text={"+54 3816 204123"}
-          link={urlWhatsApp}
-          logo={wppLogo}
-          logoName={"WhatsApp"}
-        />
-        <Contact
-          text={"santirubiol14@gmail.com"}
-          link={urlCorreo}
-          logo={gmailLogo}
-          logoName={"Mail"}
-        />
-        <Contact
-          text={"Linkedin"}
-          link={"https://www.linkedin.com/in/santiago-rubiol/"}
-          logo={linkedinLogo}
-          logoName={"Linkedin"}
-        />
-        <Contact
-          text={"GitHub"}
-          link={"https://github.com/Santi-Rubiol"}
-          logo={gitHubLogo}
-          logoName={"GitHub"}
-        />
-      </div>
-      <div className="skillsContent">
-        <Skill name={"Javascript"} image={"https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png"} />
+    <div className="container-fluid principalPage">
+      <div className="row">
+        <div className="col-2 sideBar">
+          AAA
+        </div>
+        <div className="col-10">
+          <header className="header">
+            <div className="headerPrincipalData">
+              <h1 className="myName">SANTIAGO RUBIOL</h1>
+              <h2 className="myTitle">FULL STACK DEVELOPER <b>(STACK MERN)</b></h2>
+            </div>
+          </header>
 
-        <Skill name={"Python"} image={"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Python_logo_51.svg/750px-Python_logo_51.svg.png?20210510195343"} />
+          <main className="main">
+            <h2>Sobre Mí</h2>
+            <div className="aboutMe">
+              <p>
+                Egresado de la <b>Universidad Tecnológica Nacional</b>, busco nuevos retos constantemente, me encanta ponerme a prueba y aprender cosas nuevas, soy una persona muy curiosa. Me apasionan las matemáticas.
+                <br />
+                Cuento con sólidos conocimientos en la comprensión de algoritmos
+                complejos y versatilidad y lógica para resolver diversos problemas, respaldados por años de experiencia en programación. Soy una persona dispuesta y capaz de realizar cualquier tarea.
+              </p>
+            </div>
+
+            <div>
+              <h2>Experiencia Laboral</h2>
+              <Experience
+                title={"AA"}
+                rol={"BB"}
+                time={"CC"}
+                text={"DD"} />
+            </div>
+
+            <div>
+              <h2>Mis Skills</h2>
+              <li className="skillsContent">
+                {skills.map((skill, index) => (
+                  <Skill key={index} name={skill.name} image={skill.image} />
+                ))}
+              </li>
+            </div>
+
+          </main>
+          <footer className="footer">
+            <div className="footerContactData">
+              {/* <p>Tafí Viejo, Tucumán, Argentina</p> */}
+              <h3>¡Contáctame!</h3>
+              { }
+              <Contact
+                text={"+54 3816 204123"}
+                link={urlWhatsApp}
+                logo={wppLogo}
+                logoName={"WhatsApp"}
+              />
+              <Contact
+                text={"santirubiol14@gmail.com"}
+                link={urlCorreo}
+                logo={gmailLogo}
+                logoName={"Mail"}
+              />
+              <Contact
+                text={"Linkedin"}
+                link={"https://www.linkedin.com/in/santiago-rubiol/"}
+                logo={linkedinLogo}
+                logoName={"Linkedin"}
+              />
+              <Contact
+                text={"GitHub"}
+                link={"https://github.com/Santi-Rubiol"}
+                logo={gitHubLogo}
+                logoName={"GitHub"}
+              />
+            </div>
+          </footer>
+        </div>
+
+
       </div>
+
     </div>
   )
 }
