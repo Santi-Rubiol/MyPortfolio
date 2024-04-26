5ad245eb579d698208aa89d34d1bb2a913a604ff
SantiRubiol <santirubiol14@gmail.com>
2024-04-26 15:46:49 -0300

    Agrego datos de experiencia laboral y creo su propio componente

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,17 +1,14 @@
+import './experience.css'
 
 const Experience = ({ title, rol, time, text }) => {
-    return <>
-        <div style={{
-            display: 'grid',
-            justifyContent: 'center',
-            alignItems: 'start',
-        }}>
+    return <ul>
+        <div className='experienceHeader'>
             <h3>{title}</h3>
             <h4>{time}</h4>
         </div>
         <h4>{rol}</h4>
         <p>{text}</p>
-    </>
+    </ul>
 
 }
 
