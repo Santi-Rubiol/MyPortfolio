06e5752546d8ef0a93e1715611c93203d4156dbd
SantiRubiol <santirubiol14@gmail.com>
2024-04-30 14:37:35 -0300

    Soluciono problema de bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,22 @@ import './style.css'
 
 function App() {
   return (
-    <div className="container-fluid principalPage">
+    <div className="principalPage container-fluid">
       <div className="row">
-        <div className="col-2 sideBar">
+        <div className="col-12 column">
+          <Header />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-2 col-md-3 col-sm-4 col-12 sideBar">
           <SideBar />
         </div>
-        <div className="col-10 principalContent">
-          <Header />
+        <div className="col-lg-10 col-md-9 col-sm-8 col-12 principalContent column">
           <Content />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 column">
           <Footer />
         </div>
       </div>
